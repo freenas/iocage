4544e0db060f27a82d99e2010a71397600ff3e01
Brandon Schneider <brandon@ixsystems.com>
2018-12-12 18:13:31 -0600

    Remove extra space in downloading fetch message

--- iocage_lib/ioc_fetch.py
+++ iocage_lib/ioc_fetch.py
@@ -777,7 +777,7 @@ class IOCFetch(object):
                                 if progress != last_progress:
                                     text = self.update_progress(
                                         progress,
-                                        f'Downloading : {f}',
+                                        f'Downloading: {f}',
                                         elapsed,
                                         chunk_size
                                     )
